dbaff7fc0a3ef7c68056e6168a15ab2f7fffc358
zexofile-shop <zexofile@gmail.com>
2026-01-25 18:03:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/remix-of-remix-of-remix-of-remix-of-remix-of-single-page-store/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
